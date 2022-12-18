6ae320d5bf4e6bbf05064d57874ab75725e7e87d
Matthieu Pays <pays.matthieuic@gmail.com>
2022-12-18 22:53:42 +0100

    Extract html and css from products in a VueJs component

--- webpack.config.js
+++ webpack.config.js
@@ -34,8 +34,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('home', './assets/home.js')
     .addStyleEntry('reset', './assets/styles/reset.css')
-    .addStyleEntry('home', './assets/styles/home.css')
     .addStyleEntry('contact', './assets/styles/contact.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
